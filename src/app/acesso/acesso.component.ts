bba70fb9704bedfbca034b94431d7da29e06ddd1
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-26 15:46:12 -0200

    Aula 251. Estado especial void e estilos na transição de estado - Banner

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {trigger,state,style,animate,transition} from '@angular/animations'
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations:[
+    trigger('animacao-banner',[
+      state('criado',style({
+        opacity:1
+      })),
+      transition('void =>criado',[
+        style({opacity:0,transform:'translate(-50px,0)'}),
+        animate('500ms 0s ease-in-out') // duração,delay e aceleração(easing)
+      ])
+
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+  public estadoBanner:string = 'criado' 
 
   constructor() { }
 
