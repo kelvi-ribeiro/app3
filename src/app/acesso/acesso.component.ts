88d1660e42df5c8e666abeab2cf35777bcd27182
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-26 19:15:43 -0200

    Aula 257. Customizando a animação do painel(Login e Cadastro) com keyFrames

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger,state,style,animate,transition} from '@angular/animations'
+import {trigger,state,style,animate,transition,keyframes} from '@angular/animations'
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
@@ -19,9 +19,22 @@ import {trigger,state,style,animate,transition} from '@angular/animations'
       state('criado',style({
         opacity:1
       })),
+      //0 VOID CRIADO ---------X-----------------------X--X--X-------------------------- XCROADP1.5//
       transition('void=>criado',[
         style({opacity:0,transform:'translate(50px,0)'}),
-        animate('500ms 0s ease-in-out') // duração,delay e aceleração(easing)
+        animate('1500ms 0s ease-in-out',keyframes([
+          style({offset:0.15,opacity:1,transform:'translateX(0)'}),
+          style({offset:0.86,opacity:1,transform:'translateX(0)'}),
+
+          style({offset:0.88,opacity:1,transform:'translateY(-10px)'}),
+          style({offset:0.90,opacity:1,transform:'translateY(10px)'}),
+          style({offset:0.92,opacity:1,transform:'translateY(-10px)'}),
+          style({offset:0.94,opacity:1,transform:'translateY(10px)'}),
+          style({offset:0.96,opacity:1,transform:'translateY(-10px)'}),
+          style({offset:0.98,opacity:1,transform:'translateY(10px)'}),
+
+          
+        ])) // duração,delay e aceleração(easing)
       ])
     ])
   ]
