6e78859ae9cffe62891bf504b2b267dccade1b64
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-26 21:25:32 -0200

    Aula 265. Conectando o Firebase SDK do projeto com o Back-End do Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  ngOnInit():void{
+    var config = {
+      apiKey: "AIzaSyBUbCyIe0lH3KW6pvrBYxW11TZP5kaWW2s",
+      authDomain: "jta-instagram-clone-d8149.firebaseapp.com",
+      databaseURL: "https://jta-instagram-clone-d8149.firebaseio.com",
+      projectId: "jta-instagram-clone-d8149",
+      storageBucket: "jta-instagram-clone-d8149.appspot.com",
+      messagingSenderId: "467604130286"
+    };
+    firebase.initializeApp(config)
+  }
 }
