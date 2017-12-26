d09c29ea80d9f64d74ad20abf0ceba8fb397c554
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-26 14:35:08 -0200

    Aula 249. Criando um model para imagens do banner e a instaciando na classe do banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,state,style,transition,animate } from '@angular/core/';
+import {Imagem} from './imagem.model'
 
 
 @Component({
@@ -26,10 +27,18 @@ import { trigger,state,style,transition,animate } from '@angular/core/';
 export class BannerComponent implements OnInit {
 
   public estado:string = 'escondido'
+  public imagens:Imagem[] = [
+    {estado:'escondido',url:'/assets/banner-acesso/img_1.png' },
+    {estado:'escondido',url:'/assets/banner-acesso/img_2.png' },
+    {estado:'visivel',url:'/assets/banner-acesso/img_3.png' },
+    {estado:'escondido',url:'/assets/banner-acesso/img_4.png' },
+    {estado:'escondido',url:'/assets/banner-acesso/img_5.png' }
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.imagens)
   }
   
   public toogleEstadoAnimacao():void{
