ba74a819d14d4582ccb6bd86eaecad3ca034eac9
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-27 16:46:54 -0200

    Aula 275. Redirecionando para rota home após login

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,9 +1,13 @@
+import{Injectable} from '@angular/core'
+import{Router} from '@angular/router'
 import { Usuario } from './acesso/usuario.model'
 import * as firebase from 'firebase'
 
+@Injectable()
 export class Autenticacao{
 
     public token_id:string
+    constructor(private router:Router){}
 
     public cadastrarUsuario(usuario: Usuario): Promise<any> {
         //console.log('Chegamos até o serviço: ', usuario)
@@ -31,7 +35,7 @@ export class Autenticacao{
                 firebase.auth().currentUser.getIdToken()
                 .then((idToken:string)=>{
                     this.token_id = idToken
-                    console.log(this.token_id)
+                   this.router.navigate(['/home'])
                 })
             })
             .catch((error: Error) => console.log(error))
