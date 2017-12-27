50fcbacbe8dbc2769422c8557a948470b7dd6e9d
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-27 20:24:51 -0200

    Aula 278. Utilizando LocalStorage para armazenamento do Token de autentificação

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -35,6 +35,7 @@ export class Autenticacao{
                 firebase.auth().currentUser.getIdToken()
                 .then((idToken:string)=>{
                     this.token_id = idToken
+                    localStorage.setItem('idToken',idToken)
                    this.router.navigate(['/home'])
                 })
             })
@@ -42,6 +43,10 @@ export class Autenticacao{
     }
 
     public autenticado():boolean{
+        if(this.token_id===undefined && localStorage.getItem('idToken')!==null ){
+            this.token_id =  localStorage.getItem('idToken')
+
+        }
         return this.token_id !==undefined
     }
 }
